7b8dc1d8900dc5a014081576446b9ddc6033ca63
Paola Lopez <paolac.lopez17@gmail.com>
2022-07-15 12:56:38 -0500

    Ruta Hola y Adios añadida

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
-app use(express.static(__dirname + '/public'));
+
+app.get('/hola', (req, res) =>{
+    res.send('ruta Hola');
+});
+
+app.get('/hola', (req, res) =>{
+    res.send('ruta Adios');
+});
+
+
+app use(express.static(__dirname + '/Course'));
 app listen(3000, ()=> {
     console.log('Example app listening on port 3000!');
 });
