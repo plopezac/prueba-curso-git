6392f1335732283e8febebc4b5c6dc779ebae9be
Paola Lopez <paolac.lopez17@gmail.com>
2022-08-11 16:31:31 -0500

    nueva ruta de registro

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.get('/adios', (req,res)=>{
     res.send('ruta Adios');
 });
 
+app.post('/login', (req, res)=> {
+    res.send('ruta de login nueva');
+});
+
+app.post('/registro', (req,res)=>{
+    res.send('recibiendo informacion de registro');
+});
+
+
 app use(express.static(__dirname + '/Course'));
 app listen(3000, ()=> {
     console.log('Example app listening on port 3000!');
